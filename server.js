5d14a3612a259fb7fc9cd93e5cee3e33d79cd92b
rayct <rayct@icloud.com>
2021-11-30 01:12:17 +0000

    Info Sec with HelmetJS - 14/14 - Course Completed

--- server.js
+++ server.js
@@ -11,13 +11,23 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash)
+    /*Store hash in your db*/
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+        console.log(res)
+        /*res == true or false*/
+    })
+})
 
 
 //END_ASYNC
 
 //START_SYNC
 
+let hash = bcrypt.hashSync(myPlaintextPassword, saltRounds)
+
+let result = bcrypt.compareSync(myPlaintextPassword, hash)
 
 
 //END_SYNC
--- test.js
+++ test.js
@@ -1,4 +1,22 @@
-// Password Hasing Algorithm
+// Password Hasing Algorithm - Hash and Compare Passwords Asynchronously
+// let bcrypt = require('bcrypt')
+
+// let input = 'dog'
+// let saltRounds = 8
+
+// let salt = bcrypt.genSaltSync(8)
+// console.log('\nSalt : ' + salt)
+
+// bcrypt.hash(input, salt, (error, hash) => {
+//     // console.log(hash)
+//     console.log('\n' + hash + '\n')
+
+//     bcrypt.compare(input, hash, (error, result) => {
+//         console.log('Result is ' + result + '\n')
+//     })
+// })
+
+// Password Hasing Algorithm - Hash and Compare Passwords Synchronously
 let bcrypt = require('bcrypt')
 
 let input = 'dog'
@@ -7,12 +25,8 @@ let saltRounds = 8
 let salt = bcrypt.genSaltSync(8)
 console.log('\nSalt : ' + salt)
 
-bcrypt.hash(input, salt, (error, hash) => {
-    // console.log(hash)
-    console.log('\n' + hash + '\n')
+let hash = bcrypt.hashSync(inputs, saltRounds);
+console.log('\nHash : ' + hash)
 
-    bcrypt.compare(input, hash, (error, result) => {
-        console.log('Result is ' + result + '\n')
-    })
-
-})
+let result = bcrypt.compareSync(input, hash)
+console.log('\n' + result + '\n')
